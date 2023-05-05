30b52f0c0b4bb5afaf6b1a06f84043f787bccc3a
nawodyaishan <nawodyain@gmail.com>
2023-05-06 00:46:17 +0530

    feat: add gui flow with multiple windows

--- spotify_playlist_gui.py
+++ spotify_playlist_gui.py
@@ -1,60 +1,64 @@
-import os
 import tkinter as tk
 from tkinter import ttk
-from dotenv import load_dotenv
+from tkinter import messagebox
 from utils import create_playlist
 
-load_dotenv()
 
-
-# Creating a class for the main application window that inherits from tk.Tk
 class SpotifyPlaylistCreatorApp(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.title("Spotify Playlist Creator")
-        self.geometry("400x200")
+        # Welcome message
+        welcome_message = "Welcome to Spotify Playlist Creator! Use this app to create custom playlists based on your top tracks."
+        messagebox.showinfo("Welcome", welcome_message)
 
-        self.create_widgets()
-
-    # create and configure the necessary widgets
-    def create_widgets(self):
-        # Time range label and combobox
-        time_range_label = ttk.Label(self, text="Time Range:")
-        time_range_label.grid(column=0, row=0, padx=10, pady=10, sticky="W")
-
+        # Time range dropdown
         self.time_range_var = tk.StringVar()
-        time_range_combobox = ttk.Combobox(self, textvariable=self.time_range_var)
-        time_range_combobox["values"] = ("All-time", "Last 6 months", "Last month")
-        time_range_combobox.current(0)
-        time_range_combobox.grid(column=1, row=0, padx=10, pady=10, sticky="W")
+        time_range_label = ttk.Label(self, text="Select time range:")
+        time_range_label.grid(row=0, column=0, padx=10, pady=10)
+        time_range_dropdown = ttk.Combobox(self, textvariable=self.time_range_var)
+        time_range_dropdown["values"] = ("long_term", "medium_term", "short_term")
+        time_range_dropdown.grid(row=0, column=1, padx=10, pady=10)
 
-        # Number of tracks label and entry
-        num_tracks_label = ttk.Label(self, text="Number of Tracks:")
-        num_tracks_label.grid(column=0, row=1, padx=10, pady=10, sticky="W")
-
+        # Number of tracks entry
         self.num_tracks_var = tk.StringVar()
+        num_tracks_label = ttk.Label(self, text="Enter number of tracks:")
+        num_tracks_label.grid(row=1, column=0, padx=10, pady=10)
         num_tracks_entry = ttk.Entry(self, textvariable=self.num_tracks_var)
-        num_tracks_entry.grid(column=1, row=1, padx=10, pady=10, sticky="W")
+        num_tracks_entry.grid(row=1, column=1, padx=10, pady=10)
 
         # Create playlist button
-        create_playlist_button = ttk.Button(self, text="Create Playlist", command=self.create_playlist)
-        create_playlist_button.grid(column=1, row=2, padx=10, pady=10)
+        create_playlist_button = ttk.Button(self, text="Create Playlist", command=self.create_playlist_gui)
+        create_playlist_button.grid(row=2, column=1, padx=10, pady=10)
 
-        #
+        # Credits button
+        credits_button = ttk.Button(self, text="Credits", command=self.show_credits)
+        credits_button.grid(row=3, column=1, padx=10, pady=10)
 
-    def create_playlist(self):
-        time_ranges = {
-            "All-time": "long_term",
-            "Last 6 months": "medium_term",
-            "Last month": "short_term",
-        }
-        time_range = time_ranges[self.time_range_var.get()]
+        # Exit confirmation
+        self.protocol("WM_DELETE_WINDOW", self.on_exit)
+
+    # Function to create a playlist with selected options
+    def create_playlist_gui(self):
+        time_range = self.time_range_var.get()
         num_tracks = int(self.num_tracks_var.get())
+        message = create_playlist(time_range, num_tracks)
+        messagebox.showinfo("Success", message)
 
-        create_playlist(time_range, num_tracks)
+    # Function to show credits
+    def show_credits(self):
+        credits = "Spotify Playlist Creator\nDeveloped by\nGitHub: \nnawodyaishan"
+        messagebox.showinfo("Credits", credits)
+
+    # Function to show exit confirmation
+    def on_exit(self):
+        if messagebox.askyesno("Exit", "Are you sure you want to exit?"):
+            self.destroy()
 
 
 if __name__ == "__main__":
     app = SpotifyPlaylistCreatorApp()
+    app.title("Spotify Playlist Creator")
+    app.geometry("400x200")
+    app.resizable(False, False)
     app.mainloop()
